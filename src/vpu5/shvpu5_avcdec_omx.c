27ea877c290c0cc499fe7896648164f20c7d8c43
Damian Hobson-Garcia <dhobsong@igel.co.jp>
2012-11-21 19:45:03 +0900

    vpu5: adjust input buffer handling to return buffers to client sooner
    Previously input buffers were only returned to the client after a full
    picture's worth of data was recieved and processed.  This poses problems when
    a picture is made up of a number of small buffers, instead of one big one.
    
    Adjust the processins so that once all of the NAL units in a buffer have
    been processed, the contents are immediately copied out and the buffer
    is returned to the client.

--- src/vpu5/shvpu5_avcdec_omx.c
+++ src/vpu5/shvpu5_avcdec_omx.c
@@ -974,6 +974,7 @@ shvpu_avcdec_BufferMgmtFunction(void *param)
 	tsem_t *pPicSem = shvpu_avcdec_Private->pPicSem;
 	queue_t processInBufQueue;
 	nal_t *pNal = NULL;
+	pic_t *pPic = NULL;
 	int ret;
 
 	shvpu_avcdec_Private->bellagioThreads->nThreadBufferMngtID =
@@ -1037,6 +1038,7 @@ shvpu_avcdec_BufferMgmtFunction(void *param)
 		    (shvpu_avcdec_Private->bIsEOSReached == OMX_FALSE))
 			pNal = parseBuffer(pComponent,
 					   pNal,
+					   &pPic,
 					   &isInBufferNeeded);
 
 		/*When we have input buffer to process then get
